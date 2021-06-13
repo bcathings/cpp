06c6ab264d0f3b2ed0b77f8fe74c3ea0c0675773
bcathings <bcathings@gmail.com>
2021-06-13 19:23:19 +0530

    cycle5-5

---
+++ lab_cycles/05/cycle5-5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Example{
+    public:
+        int i;
+        Example(int j){
+            i = j;
+        }
+        ~Example(){
+            cout<<"Class "<<i<<" Destructor called"<<endl;
+        }
+};
+int main(){
+    // escape from block
+    {
+        Example obj(1);
+    }
+    // Escape from function
+    Example obj(2);
+    return 0;
+}
+// escape from program
+Example obj(3);
