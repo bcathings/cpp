8823d4e1718985731a0862e6187bf82235e9820c
roshan472 <83596663+roshan472@users.noreply.github.com>
2021-06-13 14:06:43 +0530

    nil (#4)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete cycle1-9.cpp
    
    * Add files via upload
    
    * Delete cycle1-7.cpp
    
    * Delete cycle1-10.cpp
    
    * Delete cycle1-3.cpp
    
    * Delete cycle1-4.cpp
    
    * Delete cycle1-6.cpp
    
    * Delete cycle1-5.cpp
    
    * Delete cycle1-10.cpp
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update cycle4-3.cpp

---
+++ lab_cycles/04/cycle4-1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class avg
+{
+int n1,n2;
+public:
+void getdata()
+{
+cout<<"Enter value 1: ";
+cin>>n1;
+cout<<"Enter value 2: ";
+cin>>n2;
+}
+friend float mean (avg m);
+};
+float mean(avg m)
+{
+return float(m.n1+ m.n2)/2;
+}
+int main()
+{
+avg m;
+m.getdata();
+cout<<"Average = "<<mean(m);
+return 0;
+}
---
+++ lab_cycles/first/cycle1-8.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int &lar(int &p,int &q)
+{
+if(p>q)
+return p;
+else
+return q;
+}
+int main()
+{
+int a,b;
+cout<<"Largest of 2 Numbers using return by reference\n";
+cout<<"-----------------------------\n";
+cout<<"Enter 2 Numbers:";
+cin>>a>>b;
+int l=lar(a,b);
+cout<<"Large="<<l;
+return 0;
+}
